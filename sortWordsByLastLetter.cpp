2ebc62ee14f6f71c5e8078822979341f185748b0
SATHISH KUMAR <69998192+sathishsk25@users.noreply.github.com>
2022-02-21 20:40:35 +0530

    Add files via upload

---
+++ sortWordsByLastLetter.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+    char temp[101],str[1001][101];
+    int i,j,N=0;
+    while(scanf("%s ",&str[N])==1){
+        N++;
+    }
+
+    for(i=0; i<N-1; i++){
+        for(j=0; j<N-1-i; j++){
+            char l1 = str[j][strlen(str[j])-1];
+            char l2 = str[j+1][strlen(str[j+1])-1];
+            if(l1 > l2){
+            strcpy(temp,str[j]);
+            strcpy(str[j],str[j+1]);
+            strcpy(str[j+1],temp);
+        }
+        }
+    }
+
+    for(i=0; i<N; i++)
+    cout << str[i] << " ";
+
+}
